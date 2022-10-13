6e76331037bf4121679554f7f78d418f50a48090
atanu <atanuj383@gmail.com>
2022-10-14 01:12:40 +0530

    api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1000;
+const port = process.env.PORT || 1000;
 
 app.get('/', (req, res) => {
   res.send('IoT Based Attendance system');
